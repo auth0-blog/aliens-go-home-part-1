16f242db2c9e2c791ed0a16a612bd7857c738752
Bruno Krebs <bruno.krebs@auth0.com>
2018-01-29 11:08:45 -0200

    centering the origin axis; making canvas fit the screen;

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 const Canvas = () => {
-  const style = {
-    border: '1px solid black',
-  };
+  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
   return (
     <svg
       id="aliens-go-home-canvas"
       preserveAspectRatio="xMaxYMax none"
-      style={style}
+      viewBox={viewBox}
     >
       <circle cx={0} cy={0} r={50} />
     </svg>
