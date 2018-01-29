4a72c2c0036d00ea8369b9081c58aa9ea940bcdc
Bruno Krebs <bruno.krebs@auth0.com>
2018-01-29 11:38:08 -0200

    removing useless stuff;

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import Canvas from './components/Canvas';
 
 class App extends Component {
@@ -9,9 +8,5 @@ class App extends Component {
     );
   }
 }
-
-App.propTypes = {
-  message: PropTypes.string.isRequired,
-};
 
 export default App;
